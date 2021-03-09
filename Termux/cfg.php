fca11e128d9a52bef2fef4572886dc3a935c3515
AndiSV <andideveloper.id@gmail.com>
2021-03-09 13:09:49 +0700

    Create cfg.php

---
+++ Termux/cfg.php
@@ -0,0 +1,18 @@
+<?php
+
+
+// data [ api.cc.lerjin.com ] \\
+
+/*-------------------------------*/
+$external_version = "2.6.1.4";
+$device = "7.1.2";
+$timezone = "8";
+$version = "42";
+/*-------------------------------*/
+$token = "MDdhOWFhZjMtNDY5NC00OTIxLThlNTYtODFkY2U3NTE3N2Ix";
+$uuid = "622445499540305";
+$lang = "in";
+$userid = "48747110";
+/*-------------------------------*/
+
+?>
