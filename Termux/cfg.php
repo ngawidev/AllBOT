722feae94b8d41c61301fdc26624001ab3c99895
AndiSV <andideveloper.id@gmail.com>
2021-03-10 12:21:15 +0700

    Update cfg.php

--- Termux/cfg.php
+++ Termux/cfg.php
@@ -9,7 +9,7 @@ $device = "7.1.2";
 $timezone = "8";
 $version = "42";
 /*-------------------------------*/
-$token = "OTJiYTI3ZjItODUzMS00YTc4LTg3OTgtZmU3M2E4MGJjNWNm";
+$token = "MjAwMzgyYTctYThhNy00N2QwLTgzYjctMGY5M2JmYzYwNTFj";
 $uuid = "622445499540305";
 $lang = "in";
 $userid = "48747110";
